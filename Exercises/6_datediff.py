2e033bf7f270dc9898166b3bbf1d37469425349b
Luftmint <arminozkalp@gmail.com>
2025-10-01 19:16:36 +0200

    datetime difference

--- Exercises/6_datediff.py
+++ Exercises/6_datediff.py
@@ -2,4 +2,8 @@
 # Count days between 2025/01/01 and the date of which this code is ran
 import datetime
 
-today = 
+d = datetime.datetime(2025, 1, 1, 00) # Sets the set date
+now = datetime.datetime.now() # Sets the current time
+diff = now - d # Subtracts the set date from current to get the value of time between
+
+print(diff) # Print out the difference
